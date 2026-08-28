f8e05be12e5b49a976ea1ed302c8477ae63f2f8b
nicollemr07 <rubianonicolle73@gmail.com>
2026-08-28 22:46:15 +0000

    feat: conectar matriz de asientos con API y vista HTML

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from estructuras.vector import VectorPeliculas
+from estructuras.matriz import MatrizAsientos
 
 app = FastAPI(title="Cine Santa Fe")
 
@@ -17,11 +18,18 @@ app.add_middleware(
 
 # Instanciamos la estructura del catálogo
 catalogo_vector = VectorPeliculas()
+sala_asientos = MatrizAsientos(filas=5, columnas=8)
+sala_asientos.reservar(1, 3, "Ana")
 
 @app.get("/api/peliculas")
 def obtener_cartelera():
     """Ruta que devuelve el vector de películas al frontend"""
     return catalogo_vector.obtener_todas()
+
+@app.get("/api/asientos")
+def obtener_asientos():
+    """Ruta que devuelve la matriz de asientos al frontend"""
+    return sala_asientos.a_estructura_json()
 
 # Montamos las carpetas para que FastAPI pueda leer el HTML y CSS
 app.mount("/static", StaticFiles(directory="static"), name="static")
